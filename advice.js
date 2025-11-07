eb540f163a8bfdddc7c079c81a285e7a1a158f26
Deracodes1 <ezemeniachidera@gmail.com>
2025-11-07 02:52:27 -0800

    advice slip api successfully integrated

--- advice.js
+++ advice.js
@@ -1,11 +1,23 @@
 "use strict";
-
-const adviceTextEl = document.getElementById("message-content");
-const adviceTextElContainer = document.querySelector(".advice-message");
+const adviceTextEl = document.querySelector(".advice-message");
 const refreshBtn = document.querySelector(".advice-btn");
 
-refreshBtn.addEventListener("click", async function (e) {
+refreshBtn.addEventListener("click", async (e) => {
   e.preventDefault();
   // show the loader spinner
-  adviceTextElContainer.innerHTML = '<div class="spinner"></div>';
+  adviceTextEl.textContent = "";
+  adviceTextEl.innerHTML = '<div class="spinner"></div>';
+  try {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    if (!res.ok) {
+      throw new Error("failed to get advice. check network connection");
+    }
+    // display the new advice after some seconds
+    setTimeout(() => {
+      adviceTextEl.textContent = `"${data.slip.advice}"`;
+    }, 300);
+  } catch (error) {
+    adviceTextEl.textContent = `${error.message}. Try again!`;
+  }
 });
